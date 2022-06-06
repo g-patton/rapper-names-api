60190f18e2c867f2758f74233d55051fc1e7f0a8
Graham Patton <graham.m.patton@gmail.com>
2022-06-06 18:43:15 -0400

    'second commit'

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.get('/api/:rapperName', (request, response)=>{
     }
 });
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on ${PORT}! 
     Better go catch it...`)
 });
+
