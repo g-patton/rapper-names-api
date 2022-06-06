91bf98db86f0437194a27283478a4fb253a10c0b
Graham Patton <graham.m.patton@gmail.com>
2022-06-06 18:10:36 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const rappers = {
+    '21 savage':{
+    'age': 29,
+    'birthName': 'Shéyaa Bin Abraham-Joseph',
+    'birthlocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthlocation': 'Chicago, Illinois'
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthlocation': 'Dylan'
+    },
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersName = request.params.rapperName.toLowerCase();
+    if (rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT}! 
+    Better go catch it...`)
+});
